a21989500597b628cdcccc140d6c807d44249b4e
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-11 13:08:53 -0500

    write example 6

--- examples/06_thread_variadic.cpp
+++ examples/06_thread_variadic.cpp
@@ -9,4 +9,46 @@
 #include <iostream>
 #include <thread>
 
-// TODO: Demonstrate all of this.
+void f1(int a) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    std::cout << "I have an integer: " << a << std::endl;
+}
+
+void f2(int a, char b) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "I have an integer and a char: " << a << ", " << b << std::endl;
+}
+
+void f3(int & a) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    std::cout << "I am f3. I shall change my integer from " << a << " to " << ++a << std::endl;
+}
+
+int main(){
+
+    std::thread t1(f1,42);
+    std::thread t2(f2,74,'k');
+
+    // std::thread t4(f1,"peup"); // ERROR: wrong parameter type for f1
+    // std::thread t5(f2,74); // ERROR: wrong number of parameters for f2
+
+    int x = 5;
+
+    std::thread t3(f3,std::ref(x));
+
+    // std::thread t4(f3,x); // ERROR: f3 wants to take x by reference
+
+
+    t1.join();
+    t2.join();
+
+    x=8;
+
+    std::cout << "Main is done with t1 and t2. Main just set x to " << x << std::endl;
+
+    t3.join();
+
+    std::cout << "x is now " << x << std::endl;
+
+    return 0;
+}
