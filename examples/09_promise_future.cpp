0a52bfe4e89d9456b0d5d75e4a3249ef9145c8c3
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-11 20:24:19 -0500

    add promise and future note

---
+++ examples/09_promise_future.cpp
@@ -0,0 +1,40 @@
+/*
+    A promise object has the potential to be filled with data.
+    It has a corresponding future object which expects to "receive" the data eventually.
+    This is a way of passing data between threads. 
+
+    The future corresponding to a promise can be obtained via the "get_future" method.
+    A promise cannot be copied, only moved. It is sort of like a unique_ptr in that regard.
+    Once a promise sets the value of its resource via the "set_value" method,
+    its future will be able to see that value.
+
+    Call the "get" method of the future will block until a value is set by the promise,
+    and then it will return that value. This is why it's useful in concurrent programming.
+    The get method can only be called *once*. It is actually moving the data, not copying it.
+*/
+
+#include <iostream>
+#include <thread>
+#include <future>
+
+void fill_in_forty_two(std::promise<int> a) {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    a.set_value(42);
+}
+
+int main() {
+
+    std::promise<int> fill_me_with_forty_two;
+    std::future<int> f = fill_me_with_forty_two.get_future();
+
+    std::thread t(fill_in_forty_two, std::move(fill_me_with_forty_two)); // promises have to be moved, not copied
+
+    std::cout << "The worker thread that should be sending us 42 has started..." << std::endl;
+
+    int hopefully_forty_two = f.get(); // blocks until a.set_value happens
+    std::cout << "The promise has finally been fullfilled! We have: " << hopefully_forty_two << std::endl;
+
+    t.join();
+
+    return 0;
+}
