5148e9ee1d1293663d2d2988dfe40c8167793b07
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-13 10:40:32 -0500

    add lock_guard example

--- examples/12_lock_guard.cpp
+++ examples/12_lock_guard.cpp
@@ -1,6 +1,13 @@
 /*
-    Now we introduce a mutex to save the day.
-    main() is unchanged
+    Actually it is usually best to avoid direct calls to lock!
+    Neglecting to unlock a mutex can lead to *deadlock*, which is very bad.
+    And it happens easily:
+    - if you accidentally return early before you get to the unlock
+    - if an exception is raised while under the lock
+
+    Another form of deadlock can occur when various threads wait for each other to release resources before releasing their own resources.
+    
+    One way to mitigate the first form of deadlock is to use a lock_guard object.
 */
 
 #include <iostream>
@@ -14,9 +21,6 @@ class WrappedInteger {
 
     int x{};
     mutable std::mutex mutex;
-    // mutexes want to be mutable!
-    // https://channel9.msdn.com/posts/C-and-Beyond-2012-Herb-Sutter-You-dont-know-blank-and-blank
-    // If we didn't say mutable, then we would get a compile error from saying that the print method below is const
 
 public:
 
@@ -24,22 +28,19 @@ public:
         // Let's pretend that a long computation with many intermediate steps
         // is needed to increment the underlying integer x. We simulate this as follows:
 
-        mutex.lock(); // This will block the current thread until the mutex is an unlocked state so that this thread can lock it.
+        std::lock_guard<std::mutex> lck(mutex); // Construtor of lock_guard automatically locks the mutex.
         int old_x = x;
         std::this_thread::sleep_for(std::chrono::microseconds(1));
         x = old_x + 1;
-        mutex.unlock(); // Unlock the mutex, potentially unblocking a thread that might be waiting to lock it.
+        
+        // No need to manually unlock now! Destructor of lock_guard will do it.
 
     }
 
     void print() const {
-        mutex.lock();
-        // print is reading and not writing data, so why lock?
-        // It's because some other thread might be writing, and if we read while another thread
-        // is writing then that is also a data race and also a potential bug.
-        // It just happens not to happen in the main() of this particular example.
+        std::lock_guard<std::mutex> lck(mutex); // Construtor of lock_guard automatically locks the mutex.
         std::cout << "The underlying integer is now: " << x << std::endl;
-        mutex.unlock();
+        // No need to manually unlock now! Destructor of lock_guard will do it.
     }
 
 
